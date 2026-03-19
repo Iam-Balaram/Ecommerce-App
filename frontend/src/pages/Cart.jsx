64c9772c9a25434e6d174adbbc8cda3ae8b150da
balarammukhi245-create <balarammukhi245@gmail.com>
2026-03-19 17:54:38 +0530

    feat: add CartTotal component to display cart totals in Cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import { CartTotal } from "../components/CartTotal";
 
 const Cart = () => {
 
@@ -53,6 +54,13 @@ const Cart = () => {
             )
           })
         }
+      </div> 
+      <div className="flex justify-end my-20">
+        <div className="w-full sm:w-112.5">
+          <CartTotal
+          />
+        </div>
+
       </div>
     </div>
   );
