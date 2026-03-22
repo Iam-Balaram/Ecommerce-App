f046be43c7bf79ca3790c4d5b61d489d95e3fc7f
balarammukhi245-create <balarammukhi245@gmail.com>
2026-03-22 20:15:10 +0530

    feat: implement MongoDB connection and error handling, add utility classes for API responses and async handling

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+ import express from "express";
+ import cors from "cors";
+ import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+
+
+ //APP config `
+ const app = express();
+ const port = process.env.PORT || 4000;
+ connectDB();
+
+ //middleware
+ app.use(express.json());
+ app.use(cors());
+
+ // api endpounts
+ app.get("/", (req,res)=>{
+    res.send("API Is Working")
+ })
+
+ app.listen(port, ()=> console.log(`Server runing on Port: ${port}`))
