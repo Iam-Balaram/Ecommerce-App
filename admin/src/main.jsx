71e6ce4f77535efa27bed7cc2ef40eb44fda631a
balarammukhi245-create <balarammukhi245@gmail.com>
2026-03-30 09:10:39 +0530

    add Navbar and Sidebar components; update App component structure

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,9 +1,11 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+ReactDom.createRoot(document.getElementById('root')).render(
 <BrowserRouter>
-<App/>
+    <App/>
 </BrowserRouter>
 )
